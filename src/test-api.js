8d2ca9e528b4e6a6727668d69147de24f348de02
RaptorCentauri <dave.leo.shilander@icloud.com>
2024-05-17 13:20:22 -0500

    fixed issue with css not loading

--- src/test-api.js
+++ src/test-api.js
@@ -1,54 +1,59 @@
-// const apiEndpoint = "http://localhost:8080"
+
+
+console.log("API HEALTH TEST");
+
+async function healthCheck() {
+	try {
+		let response = await fetch("/api/health", {
+			method: "GET",
+			headers: {}
+		});
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch health status");
+		}
+
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error("Error performing health check:", error);
+	}
+}
+
+healthCheck().then(r => {}); // Call the async function
 
 
 
-// fetch("http://localhost:8080/student/all", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"Authorization": "Bearer",
-// 		"Cookie": "JSESSIONID=221FFF3E607AA4D6998C5F1E4EB44EE0"
+
+// attempt login and JWT retrieval
+
+
+// async function loginUser(){
+// 	try{
+// 		const response = await fetch("http://localhost:8081/api/auth/login",
+// 			{
+// 			"method": "POST",
+// 			"headers": {
+// 				"Content-Type": "application/json; charset=utf-8"
+// 			},
+// 			"body": "{\"username\":\"dshiland\",\"password\":\"password\"}"
+// 		})
+//
+// 		if(!response.ok){
+// 			throw new Error("Unable to login")
+// 		}
+//
+// 		const {accessToken} = await response.json()
 // 	}
-// })
-// 	.then((res) => res.text())
-// 	.then(console.log.bind(console))
-// 	.catch(console.error.bind(console));
+// }
 
 
-console.log("API SCRIPT TEST")
-
-
-fetch("/api/health", {
-	"method": "GET",
-	"headers": {}
-})
-	.then((res) => res.text())
-	.then(console.log.bind(console))
-	.catch(console.error.bind(console));
-
-
-
-// fetch("http://dolphintellect-api:8080/api/health", {
-// 	"method": "GET",
-// 	"headers": {}
-// })
-// 	.then((res) => res.text())
-// 	.then(console.log.bind(console))
-// 	.catch(console.error.bind(console));
-
-
-
-// fetch("http://0.0.0.0:8080/api/health", {
-// 	"method": "GET",
-// 	"headers": {}
-// })
-// 	.then((res) => res.text())
-// 	.then(console.log.bind(console))
-// 	.catch(console.error.bind(console));
-
-
-// fetch("http://localhost:8081/api/health", {
-// 	"method": "GET",
-// 	"headers": {}
+// fetch("http://localhost:8081/api/auth/login", {
+// 	"method": "POST",
+// 	"headers": {
+// 		"Content-Type": "application/json; charset=utf-8"
+// 	},
+// 	"body": "{\"username\":\"dshiland\",\"password\":\"password\"}"
 // })
 // 	.then((res) => res.text())
 // 	.then(console.log.bind(console))
